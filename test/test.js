767eda27e740ca2efa277396bebb56a270d6ae77
Johan Satgé <johansatge@users.noreply.github.com>
2016-04-23 12:37:00 +0200

    Checks EXIF integrity in tests

--- test/test.js
+++ test/test.js
@@ -1,25 +1,22 @@
 
 var should = require('chai').should();
+var fs = require('fs');
 var exec = require('child_process').exec;
+var piexif = require('piexifjs');
+var jpegjs = require('jpeg-js');
 var jo = require('../src/main.js');
 
 describe('jpeg-autorotate', function()
 {
 
-    [2, 3, 4, 5, 6, 7, 8].map(function(o)
-    {
-        it('Should rotate image with orientation ' + o, function(done)
-        {
-            jo.rotate(__dirname + '/images/image_' + o + '.jpg', {}, function(error, buffer, orientation)
-            {
-                if (error)
-                {
-                    throw error;
-                }
-                done();
-            });
-        });
-    });
+    itShouldTransform(__dirname + '/images/image_2.jpg', 'image_2.jpg');
+    itShouldTransform(__dirname + '/images/image_3.jpg', 'image_3.jpg');
+    itShouldTransform(__dirname + '/images/image_4.jpg', 'image_4.jpg');
+    itShouldTransform(__dirname + '/images/image_5.jpg', 'image_5.jpg');
+    itShouldTransform(__dirname + '/images/image_6.jpg', 'image_6.jpg');
+    itShouldTransform(__dirname + '/images/image_7.jpg', 'image_7.jpg');
+    itShouldTransform(__dirname + '/images/image_8.jpg', 'image_8.jpg');
+    itShouldTransform(__dirname + '/images/image_exif.jpg', 'image_exif.jpg');
 
     it('Should return an error if the orientation is 1', function(done)
     {
@@ -74,3 +71,70 @@ describe('jpeg-autorotate', function()
     // @todo test jo.errors.rotate_file (corrupted JPEG ?)
 
 });
+
+/**
+ * Tries to transform the given path, and checks data integrity (EXIF, dimensions)
+ * @param path
+ * @param label
+ */
+function itShouldTransform(path, label)
+{
+    it('Should rotate image (' + label + ')', function(done)
+    {
+        this.timeout(10000);
+        var orig_buffer = fs.readFileSync(path);
+        var orig_exif = piexif.load(orig_buffer.toString('binary'));
+        var orig_jpeg = jpegjs.decode(orig_buffer);
+        jo.rotate(path, {}, function(error, buffer, orientation)
+        {
+            if (error)
+            {
+                throw error;
+            }
+            var dest_exif = piexif.load(buffer.toString('binary'));
+            var dest_jpeg = jpegjs.decode(buffer);
+            if (orientation < 5 && (orig_jpeg.width !== dest_jpeg.width || orig_jpeg.height !== dest_jpeg.height))
+            {
+                throw new Eror('Dimensions do not match');
+            }
+            if (orientation >= 5 && (orig_jpeg.width !== dest_jpeg.height || orig_jpeg.height !== dest_jpeg.width))
+            {
+                throw new Eror('Dimensions do not match');
+            }
+            if (!compareEXIF(orig_exif, dest_exif))
+            {
+                throw new Error('EXIF do not match');
+            }
+            done();
+        });
+    });
+}
+
+/**
+ * Compares EXIF arrays
+ * The properties allowed to differ between the two versions are set to 0
+ * @param orig
+ * @param dest
+ */
+function compareEXIF(orig, dest)
+{
+    orig['thumbnail'] = 0; // The thumbnail
+    dest['thumbnail'] = 0;
+    orig['0th'][piexif.ImageIFD.Orientation] = 0; // Orientation
+    dest['0th'][piexif.ImageIFD.Orientation] = 0;
+    orig['0th'][piexif.ImageIFD.ExifTag] = 0; // Pointer to the Exif IFD
+    dest['0th'][piexif.ImageIFD.ExifTag] = 0;
+    orig['Exif'][piexif.ExifIFD.PixelXDimension] = 0; // Image width
+    dest['Exif'][piexif.ExifIFD.PixelXDimension] = 0;
+    orig['Exif'][piexif.ExifIFD.PixelYDimension] = 0; // Image height
+    dest['Exif'][piexif.ExifIFD.PixelYDimension] = 0;
+    orig['1st'][piexif.ImageIFD.JPEGInterchangeFormat] = 0; // Offset to the start byte of the thumbnail
+    dest['1st'][piexif.ImageIFD.JPEGInterchangeFormat] = 0;
+    orig['1st'][piexif.ImageIFD.JPEGInterchangeFormatLength] = 0; // Number of bytes of the thumbnail
+    dest['1st'][piexif.ImageIFD.JPEGInterchangeFormatLength] = 0;
+
+    var orig_json = JSON.stringify(orig);
+    var dest_json = JSON.stringify(dest);
+
+    return orig_json === dest_json;
+}
