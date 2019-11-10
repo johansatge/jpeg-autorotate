6178b79e7f7999536414afda1ff0bcfc83c169a9
Joshua Peterson <joshua.peterson@volusion.com>
2019-11-10 13:34:46 -0600

    fix for multiple files. Refactored out the vestigal index variable

--- src/cli.js
+++ src/cli.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
-const argv = require('yargs').argv
-const colors = require('colors')
-const fs = require('fs')
-const glob = require('glob')
-const jo = require('./main.js')
-const manifest = require('../package.json')
-const promisify = require('util').promisify
+const argv = require('yargs').argv;
+const colors = require('colors');
+const fs = require('fs');
+const glob = require('glob');
+const jo = require('./main.js');
+const manifest = require('../package.json');
+const promisify = require('util').promisify;
 
 if (argv.version) {
-  console.log(manifest.name + ' ' + manifest.version)
+  console.log(manifest.name + ' ' + manifest.version);
   process.exit(0)
 }
 
@@ -26,8 +26,8 @@ if (argv.help || typeof argv._ === 'undefined' || argv._.length === 0) {
     '--version           Output current version',
     '--help              Output help',
     '',
-  ]
-  console.log(help.join('\n'))
+  ];
+  console.log(help.join('\n'));
   process.exit(0)
 }
 
@@ -37,7 +37,7 @@ listFiles()
     process.exit(0)
   })
   .catch((error) => {
-    console.log(error.message)
+    console.log(error.message);
     process.exit(1)
   })
 
@@ -47,33 +47,36 @@ function listFiles() {
   })
 }
 
-function processFiles(files, index = 0) {
-  if (index + 1 > files.length) {
+function processFiles(files) {
+  if (files.length === 0) {
     return Promise.resolve()
   }
-  const filePath = files[index]
-  return jo
-    .rotate(filePath, {quality: argv.quality})
-    .then(({buffer, orientation, quality, dimensions}) => {
-      return promisify(fs.writeFile)(files[index], buffer).then(() => {
-        return {orientation, quality, dimensions}
-      })
-    })
-    .then(({orientation, quality, dimensions}) => {
-      const message =
-        'Processed (Orientation: ' +
-        orientation +
-        ') (Quality: ' +
-        quality +
-        '%) (Dimensions: ' +
-        dimensions.width +
-        'x' +
-        dimensions.height +
-        ')'
-      console.log(filePath + ': ' + colors.green(message))
-    })
-    .catch((error) => {
-      const isFatal = error.code !== jo.errors.correct_orientation
-      console.log(filePath + ': ' + (isFatal ? colors.red(error.message) : colors.yellow(error.message)))
-    })
+  const promiseArray = [];
+  files.forEach(filePath => {
+    promiseArray.push(jo
+        .rotate(filePath, {quality: argv.quality})
+        .then(({buffer, orientation, quality, dimensions}) => {
+          return promisify(fs.writeFile)(filePath, buffer).then(() => {
+            return {orientation, quality, dimensions}
+          })
+        })
+        .then(({orientation, quality, dimensions}) => {
+          const message =
+              'Processed (Orientation: ' +
+              orientation +
+              ') (Quality: ' +
+              quality +
+              '%) (Dimensions: ' +
+              dimensions.width +
+              'x' +
+              dimensions.height +
+              ')';
+          console.log(filePath + ': ' + colors.green(message))
+        })
+        .catch((error) => {
+          const isFatal = error.code !== jo.errors.correct_orientation;
+          console.log(filePath + ': ' + (isFatal ? colors.red(error.message) : colors.yellow(error.message)))
+        }));
+  });
+  return Promise.all(promiseArray).then (() => console.log('done processing files'));
 }
